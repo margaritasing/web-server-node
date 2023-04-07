8ada9dc124b5047a5ccbc59eaf41ee726d1f9e78
LMFM <61855440+margaritasing@users.noreply.github.com>
2023-04-07 15:41:39 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT || 3000;
+        this.port = 8080 || 3000;
         this.usuariosPath = '/api/usuarios';
 
         // Middlewares
